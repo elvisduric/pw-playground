20af4cb4bb3e810539d4e44c2653a0044759a68c
Elvis Duric <elvisduric@Elviss-MacBook-Pro-2.local>
2023-11-24 14:30:41 +0100

    Updates

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+// require('dotenv.env').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -32,7 +32,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    headless: true,
+    headless: false,
     baseURL: 'https://tutorialsninja.com/',
     apiURL: 'https://gorest.co.in/public',
 
